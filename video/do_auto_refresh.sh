f09924dc164d630a8f32313901ea4efa22081fe2
Daniel M. Lofaro <dan@danlofaro.com>
2022-07-27 05:17:44 +0900

    testing do auto refresh

--- video/do_auto_refresh.sh
+++ video/do_auto_refresh.sh
@@ -1,13 +1,11 @@
 echo 'Auto upload djap.png.cropped.color'
-THE_PASS=$(cat ~/lb-music.pass)
+THE_PASS=$(cat ~/.pass/lb-music.pass)
+THE_DIR='~/lb-music.d/'
+mkdir -p $THE_DIR
 while [ 1 -gt 0 ]
 do
-        cp djap.png.cropped.color djap_to_jpg.png
-        mogrify -format jpg djap_to_jpg.png
-        mv djap_to_jpg.png djap.png.jpg
-        sshpass -p $THE_PASS scp djap.png.jpg lovebot@love-bot.xyz:
-        sshpass -p $THE_PASS scp djap.png.cropped.color lovebot@love-bot.xyz:
+        sshpass -p $THE_PASS scp *.val lovebot@love-bot.xyz:$THE_DIR
         echo -n '.'
-        sleep 10
+        sleep 1
 done
 

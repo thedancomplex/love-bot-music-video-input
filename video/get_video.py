bbde470480a924b4fb57aa9359dd2c026f349789
Daniel M. Lofaro <dan@danlofaro.com>
2022-07-27 04:53:29 +0900

    square and has edge detection

--- video/get_video.py
+++ video/get_video.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 vid = cv2.VideoCapture(0)
 
@@ -10,8 +11,11 @@ while(True):
       ystart = None
       xend   = None
       yend   = None
-
+      xs     = None
+      yx     = None
       if (s[0] < s[1]):
+          xs   = s[0]
+          ys   = s[0]
           dxy  = s[1] - s[0]
           ddxy = int(dxy/2)
           xstart = 0    
@@ -19,6 +23,8 @@ while(True):
           ystart = 0    + ddxy
           yend   = s[0] + ddxy
       else:
+          xs   = s[1]
+          ys   = s[1]
           dxy  = s[0] - s[1]
           ddxy = int(dxy/2)
           xstart = 0    + ddxy
@@ -28,6 +34,11 @@ while(True):
       cimg = frame[xstart:xend, ystart:yend]
       s = cimg.shape
       print(s)
-      cv2.imshow('Video Capture Raw', cimg)
+      gray = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY)
+      norm = np.zeros((xs, ys))
+      final = cv2.normalize(gray, norm, 0, 255, norm_type=cv2.NORM_MINMAX)
+      sobelx = cv2.Sobel(src=final, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+      ret, thresh = cv2.threshold(final, 0, 128, cv2.THRESH_BINARY)
+      cv2.imshow('Video Capture Raw', sobelx)
       if cv2.waitKey(1) & 0xff == ord('q'):
           break
